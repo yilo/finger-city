d7346d58a543a135f9d32b8c4997eb89dcbd5a8f
yilo <successforevertim@gmail.com>
2015-11-20 11:35:21 +0800

    Update Starter.java

--- src/main/java/org/racoon/finger/city/bootstrap/Starter.java
+++ src/main/java/org/racoon/finger/city/bootstrap/Starter.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 /**
  * created by long, created on Nov 20, 2015
  */
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Starter {
 
 	public static void main(String[] args) {
