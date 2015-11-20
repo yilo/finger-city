1c08ad19a224f653e8a09f023399484235bb5dc2
yilo <successforevertim@gmail.com>
2015-11-20 15:02:21 +0800

    Create UserRepository.java

---
+++ src/main/java/org/racoon/finger/city/jpa/repos/UserRepository.java
@@ -0,0 +1,11 @@
+package org.racoon.finger.city.jpa.repos;
+
+import org.racoon.finger.city.jpa.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
