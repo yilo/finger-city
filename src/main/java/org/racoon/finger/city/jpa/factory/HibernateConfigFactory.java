4d98f2ae5891e2bd7fe8e489218083c215c72c08
yilo <successforevertim@gmail.com>
2015-11-20 15:01:36 +0800

    Create HibernateConfigFactory.java

---
+++ src/main/java/org/racoon/finger/city/jpa/factory/HibernateConfigFactory.java
@@ -0,0 +1,13 @@
+package org.racoon.finger.city.jpa.factory;
+
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+public interface HibernateConfigFactory {
+	String HIBERNATE_SCHEMA_KEY = "hibernate.default_schema";
+	String HIBERNATE_DDL_AUTO_KEY = "hibernate.hbm2ddl.auto";
+
+	HibernateJpaVendorAdapter getJpaAdapter();
+}
