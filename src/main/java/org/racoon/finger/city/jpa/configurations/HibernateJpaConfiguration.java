bb9c97a123e4900bdd8c376f7bcdbf04cd0c69c7
yilo <successforevertim@gmail.com>
2015-11-20 14:59:43 +0800

    Create HibernateJpaConfiguration.java

---
+++ src/main/java/org/racoon/finger/city/jpa/configurations/HibernateJpaConfiguration.java
@@ -0,0 +1,14 @@
+package org.racoon.finger.city.jpa.configurations;
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+
+public class HibernateJpaConfiguration extends JpaConfiguration {
+
+	public String schema;
+	public boolean showSql;
+	public String database;
+	public String entityPackageName;
+
+}
