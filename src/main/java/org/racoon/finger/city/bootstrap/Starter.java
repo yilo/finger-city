298eb203d94508e440410348262848131b311849
yilo <successforevertim@gmail.com>
2015-11-20 11:04:06 +0800

    Create Starter.java

---
+++ src/main/java/org/racoon/finger/city/bootstrap/Starter.java
@@ -0,0 +1,16 @@
+package org.racoon.finger.city.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+@EnableAutoConfiguration
+public class Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starter.class, args);
+	}
+
+}
