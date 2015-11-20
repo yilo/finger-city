ff5fb2a8b3a29da940084b26b74224b5416fb540
yilo <successforevertim@gmail.com>
2015-11-20 14:58:00 +0800

    Update Starter.java

--- src/main/java/org/racoon/finger/city/bootstrap/Starter.java
+++ src/main/java/org/racoon/finger/city/bootstrap/Starter.java
@@ -1,12 +1,16 @@
 package org.racoon.finger.city.bootstrap;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * created by long, created on Nov 20, 2015
  */
 @SpringBootApplication
+@ComponentScan(basePackages = { "org.racoon.finger.city" })
+@EnableConfigurationProperties
 public class Starter {
 
 	public static void main(String[] args) {
