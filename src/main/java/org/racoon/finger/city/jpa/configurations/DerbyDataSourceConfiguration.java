50e06ccb973592d7653c1b367f4043a4793ad7d5
yilo <successforevertim@gmail.com>
2015-11-20 14:59:00 +0800

    Create DerbyDataSourceConfiguration.java

---
+++ src/main/java/org/racoon/finger/city/jpa/configurations/DerbyDataSourceConfiguration.java
@@ -0,0 +1,13 @@
+package org.racoon.finger.city.jpa.configurations;
+
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+
+public class DerbyDataSourceConfiguration extends DataSourceConfiguration {
+
+	public String url;
+	public int minPoolSize;
+	public int maxPoolSize;
+}
