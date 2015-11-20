dae1894c1fed2149ebf6568db9512600af2b1d17
yilo <successforevertim@gmail.com>
2015-11-20 14:58:31 +0800

    Create DataSourceConfiguration.java

---
+++ src/main/java/org/racoon/finger/city/jpa/configurations/DataSourceConfiguration.java
@@ -0,0 +1,11 @@
+package org.racoon.finger.city.jpa.configurations;
+
+/**
+ * created by long, created on Nov 20, 2015
+ */
+public class DataSourceConfiguration {
+
+	public String user;
+	public String pwd;
+	public String driverName;
+}
